8e95ddd80de872e222e950ffa0e53c19cb1d70f3
Divya Prakash <prakashpreveen@gmail.com>
2024-12-17 18:06:30 +0530

    added analytics

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,8 @@ import React, { useState } from "react";
 import { AiOutlineClose, AiOutlineMenu } from "react-icons/ai";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false);
+  const [nav, setNav] = useState(true);
+  console.log(nav);
 
   const handleNav = () => {
     setNav(!nav);
