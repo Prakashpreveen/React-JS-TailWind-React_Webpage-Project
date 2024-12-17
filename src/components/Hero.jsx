9af12e26e6a67dff4b8f2efe5a3c69227484f0ad
Divya Prakash <prakashpreveen@gmail.com>
2024-12-17 18:31:58 +0530

    added newsletter

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -3,7 +3,7 @@ import { ReactTyped } from "react-typed";
 
 const Hero = () => {
   return (
-    <div className="text-white">
+    <div className="text-white px-2">
       <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
         <p className="text-[#00df9a] font-bold p-2">
           GROWING WITH DATA ANALYTICS
@@ -12,7 +12,7 @@ const Hero = () => {
           Grow with Data.
         </h1>
         <div className="flex items-center justify-center">
-          <p className="md:text-5xl sm:text-4xl text-xl font-bold py-4">
+          <p className="md:text-4xl sm:text-3xl text-xl font-bold py-4">
             Fast, Flexible, Financing for
           </p>
           <div>
